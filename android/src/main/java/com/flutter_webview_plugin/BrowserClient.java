87db484efdba08ed4d617a8b1557979b05f95452
ersa.dimitri <p@ssw0rd>
2019-09-17 23:25:19 +0700

    change onurlchange

--- android/src/main/java/com/flutter_webview_plugin/BrowserClient.java
+++ android/src/main/java/com/flutter_webview_plugin/BrowserClient.java
@@ -52,7 +52,7 @@ public class BrowserClient extends WebViewClient {
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
         Map<String, Object> data = new HashMap<>();
-        data.put("url", url);
+        data.put("url", view.getUrl);
 
         FlutterWebviewPlugin.channel.invokeMethod("onUrlChanged", data);
 
